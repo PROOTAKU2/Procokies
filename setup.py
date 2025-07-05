fa04517aaf364931b8d4cdd79f62b14535c22a52
PROOTAKU2 <prootaku99963@gmail.com>
2025-07-06 05:22:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Procookies',
-    version='0.1',
-    packages=find_packages(),
+    name="Procokies",  # your package name
+    version="1.0.0",
+    author="PROOTAKU2",
+    description="Central cookies handler for telegram music bots",
+    packages=find_packages(),  # Automatically detect packages
+    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    package_data={
+        "Procokies": ["cookies.txt"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
